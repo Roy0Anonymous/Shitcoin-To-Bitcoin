8bed462518a5696be84ef2ad2e432b2ebacb3271
Rahul Roy <rahulroy.e1004@gmail.com>
2023-04-01 18:51:45 +0530

    Added Example Images and Updated API Key

--- ShitCoinToBitcoin/Views/ContentView.swift
+++ ShitCoinToBitcoin/Views/ContentView.swift
@@ -15,183 +15,188 @@ struct ContentView: View {
     @State private var convertedValue: String = "0.0"
     
     var body: some View {
-        ZStack(alignment: .bottom) {
-            Circle()
-                .foregroundColor(.orange)
-                .frame(width: 400, height: 400)
-                .blur(radius: 100)
-                .offset(x: 0, y: 275)
-            VStack(alignment: .center, spacing: 0) {
-                HeadingView(title: "Shitcoin to Bitcoin Convertor", subtitle: "Don't just flush your coins away")
-                HStack {
-                    ZStack {
-                        Image(uiImage: compressImage(image: UIImage(imageLiteralResourceName: "Cross")))
+        ZStack {
+            Color.black
+                .ignoresSafeArea()
+            ZStack(alignment: .bottom) {
+                Circle()
+                    .foregroundColor(.orange)
+                    .frame(width: 400, height: 400)
+                    .blur(radius: 100)
+                    .offset(x: 0, y: 275)
+                VStack(alignment: .center, spacing: 0) {
+                    HeadingView(title: "Shitcoin to Bitcoin Convertor", subtitle: "Don't just flush your coins away")
+                    HStack {
+                        ZStack {
+                            Image(uiImage: compressImage(image: UIImage(imageLiteralResourceName: "Cross")))
+                                .resizable()
+                                .frame(width: 150, height: 150)
+                                .padding(.leading)
+                                .shadow(color: .red.opacity(0.5), radius: 10, x: 2, y: 7)
+                            Image(uiImage: compressImage(image: UIImage(imageLiteralResourceName: "TRX")))
+                                .resizable()
+                                .frame(width: 25, height: 25)
+                                .padding(.bottom, 90)
+                            
+                            Image(uiImage: compressImage(image: UIImage(imageLiteralResourceName: "DOGE")))
+                                .resizable()
+                                .frame(width: 25, height: 25)
+                                .padding(.trailing, 60)
+                            
+                            Image(uiImage: compressImage(image: UIImage(imageLiteralResourceName: "BCD")))
+                                .resizable()
+                                .frame(width: 25, height: 25)
+                                .padding(.leading, 10)
+                                .padding(.top, 80)
+                            
+                            Image(uiImage: compressImage(image: UIImage(imageLiteralResourceName: "SHIB")))
+                                .resizable()
+                                .frame(width: 25, height: 25)
+                                .padding(.top, 60)
+                                .padding(.trailing, 60)
+                            
+                            Image(uiImage: compressImage(image: UIImage(imageLiteralResourceName: "DCN")))
+                                .resizable()
+                                .frame(width: 25, height: 25)
+                                .padding(.leading, 100)
+                            
+                            Image(uiImage: compressImage(image: UIImage(imageLiteralResourceName: "ETN")))
+                                .resizable()
+                                .frame(width: 25, height: 25)
+                                .padding(.bottom, 80)
+                                .padding(.leading, 60)
+                            
+                            Image(uiImage: compressImage(image: UIImage(imageLiteralResourceName: "BTS")))
+                                .resizable()
+                                .frame(width: 25, height: 25)
+                                .padding(.leading, 40)
+                                .padding(.bottom, 40)
+                        }
+                        Spacer()
+                        Image(uiImage: compressImage(image: UIImage(imageLiteralResourceName: "Convert")))
+                            .resizable()
+                            .frame(width: 40, height: 40)
+                            .shadow(color: .blue.opacity(0.5), radius: 4, x: 2, y: 4)
+                        Spacer()
+                        Image(uiImage: compressImage(image: UIImage(imageLiteralResourceName: "BTC")))
                             .resizable()
                             .frame(width: 150, height: 150)
-                            .padding(.leading)
-                            .shadow(color: .red.opacity(0.5), radius: 10, x: 2, y: 7)
-                        Image(uiImage: compressImage(image: UIImage(imageLiteralResourceName: "TRX")))
-                            .resizable()
-                            .frame(width: 25, height: 25)
-                            .padding(.bottom, 90)
-
-                        Image(uiImage: compressImage(image: UIImage(imageLiteralResourceName: "DOGE")))
-                            .resizable()
-                            .frame(width: 25, height: 25)
-                            .padding(.trailing, 60)
-
-                        Image(uiImage: compressImage(image: UIImage(imageLiteralResourceName: "BCD")))
-                            .resizable()
-                            .frame(width: 25, height: 25)
-                            .padding(.leading, 10)
-                            .padding(.top, 80)
-
-                        Image(uiImage: compressImage(image: UIImage(imageLiteralResourceName: "SHIB")))
-                            .resizable()
-                            .frame(width: 25, height: 25)
-                            .padding(.top, 60)
-                            .padding(.trailing, 60)
-
-                        Image(uiImage: compressImage(image: UIImage(imageLiteralResourceName: "DCN")))
-                            .resizable()
-                            .frame(width: 25, height: 25)
-                            .padding(.leading, 100)
-
-                        Image(uiImage: compressImage(image: UIImage(imageLiteralResourceName: "ETN")))
-                            .resizable()
-                            .frame(width: 25, height: 25)
-                            .padding(.bottom, 80)
-                            .padding(.leading, 60)
-
-                        Image(uiImage: compressImage(image: UIImage(imageLiteralResourceName: "BTS")))
-                            .resizable()
-                            .frame(width: 25, height: 25)
-                            .padding(.leading, 40)
-                            .padding(.bottom, 40)
+                            .padding(.trailing)
+                            .shadow(color: .orange.opacity(0.5), radius: 10, x: 2, y: 7)
                     }
-                    Spacer()
-                    Image(uiImage: compressImage(image: UIImage(imageLiteralResourceName: "Convert")))
-                        .resizable()
-                        .frame(width: 40, height: 40)
-                        .shadow(color: .blue.opacity(0.5), radius: 4, x: 2, y: 4)
-                    Spacer()
-                    Image(uiImage: compressImage(image: UIImage(imageLiteralResourceName: "BTC")))
-                        .resizable()
-                        .frame(width: 150, height: 150)
-                        .padding(.trailing)
-                        .shadow(color: .orange.opacity(0.5), radius: 10, x: 2, y: 7)
-                }
-
-                if let data = coinManager.data {
-                    Menu {
-                        ForEach(data.data) { coin in
-                            Button(coin.name) {
-                                selectedCoin = coin.name
+                    
+                    if let data = coinManager.data {
+                        Menu {
+                            ForEach(data.data) { coin in
+                                Button(coin.name) {
+                                    selectedCoin = coin.name
+                                }
+                            }
+                        } label: {
+                            Text(selectedCoin)
+                                .padding(.trailing, 40)
+                                .padding(.leading)
+                                .frame(height: 30)
+                                .padding(.trailing, 5)
+                                .background(.orange.opacity(0.7))
+                                .foregroundColor(.white)
+                                .cornerRadius(7)
+                                .overlay (
+                                    ZStack(alignment: .trailing) {
+                                        HStack {
+                                            Spacer()
+                                            Image(systemName: "chevron.down")
+                                                .foregroundColor(.white)
+                                                .padding(.trailing)
+                                        }
+                                    }
+                                )
+                        }
+                        .frame(maxWidth: .infinity)
+                        .padding(.vertical, 15)
+                        .shadow(color: Color.orange, radius: 10, x: 5, y: 5)
+                        
+                        ZStack {
+                            TextField("", text: $textFieldValue)
+                                .multilineTextAlignment(.center)
+                                .frame(width: 150, height: 50, alignment: .center)
+                                .background(Color.white)
+                                .foregroundColor(.black)
+                                .shadow(color: .orange.opacity(0.5), radius: 15, x: 5, y: 5)
+                                .cornerRadius(10)
+                                .padding(.bottom, 30)
+                                .keyboardType(.decimalPad)
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.orange, lineWidth: 2)
+                                .shadow(color: Color.orange, radius: 10, x: 0, y: 5)
+                                .frame(width: 150, height: 50, alignment: .center)
+                                .padding(.bottom, 30)
+                            if textFieldValue.isEmpty {
+                                Text("Enter the Amount")
+                                    .frame(width: 150, height: 50, alignment: .center)
+                                    .foregroundColor(.gray.opacity(0.4))
+                                    .padding(.bottom, 30)
+                                    .allowsHitTesting(false)
                             }
                         }
-                    } label: {
-                        Text(selectedCoin)
-                            .padding(.trailing, 40)
-                            .padding(.leading)
-                            .frame(height: 30)
-                            .padding(.trailing, 5)
-                            .background(.orange.opacity(0.7))
-                            .foregroundColor(.white)
-                            .cornerRadius(7)
-                            .overlay (
-                                ZStack(alignment: .trailing) {
-                                    HStack {
-                                        Spacer()
-                                        Image(systemName: "chevron.down")
-                                            .foregroundColor(.white)
-                                            .padding(.trailing)
-                                    }
-                                }
-                            )
-                    }
-                    .frame(maxWidth: .infinity)
-                    .padding(.vertical, 15)
-                    .shadow(color: Color.orange, radius: 10, x: 5, y: 5)
-
-                    ZStack {
-                        TextField("", text: $textFieldValue)
+                        Button(action: {
+                            UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                            if selectedCoin == "Select a Coin" || textFieldValue.isEmpty {
+                                return
+                            }
+                            let currentSelection = data.data.firstIndex { currData in
+                                return currData.name == selectedCoin
+                            }
+                            let value = Double(textFieldValue)! * data.data[currentSelection!].quote.btc.price
+                            convertedValue = String(format: "%.10f", value)
+                            resultText = "You'll get \(convertedValue) BTC after recycling your trash"
+                        }, label: {
+                            Text("Get Bitcoins Now!")
+                                .foregroundColor(.white)
+                                .padding()
+                                .background(Color.orange)
+                                .cornerRadius(10)
+                                .shadow(color: .orange.opacity(0.8), radius: 10, x: 5, y: 5)
+                        })
+                        .padding(.bottom, 20)
+                        Spacer()
+                        Text(resultText)
                             .multilineTextAlignment(.center)
-                            .frame(width: 150, height: 50, alignment: .center)
-                            .background(Color.white)
-                            .foregroundColor(.black)
-                            .shadow(color: .orange.opacity(0.5), radius: 15, x: 5, y: 5)
-                            .cornerRadius(10)
-                            .padding(.bottom, 30)
-                            .keyboardType(.decimalPad)
-                        RoundedRectangle(cornerRadius: 10)
-                            .stroke(Color.orange, lineWidth: 2)
-                            .shadow(color: Color.orange, radius: 10, x: 0, y: 5)
-                            .frame(width: 150, height: 50, alignment: .center)
-                            .padding(.bottom, 30)
-                        if textFieldValue.isEmpty {
-                            Text("Enter the Amount")
-                                .frame(width: 150, height: 50, alignment: .center)
-                                .foregroundColor(.gray.opacity(0.4))
-                                .padding(.bottom, 30)
-                                .allowsHitTesting(false)
-                        }
-                    }
-                    Button(action: {
-                        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-                        if selectedCoin == "Select a Coin" || textFieldValue.isEmpty {
-                            return
-                        }
-                        let currentSelection = data.data.firstIndex { currData in
-                            return currData.name == selectedCoin
-                        }
-                        let value = Double(textFieldValue)! * data.data[currentSelection!].quote.btc.price
-                        convertedValue = String(format: "%.10f", value)
-                        resultText = "You'll get \(convertedValue) BTC after recycling your trash"
-                    }, label: {
-                        Text("Get Bitcoins Now!")
+                            .font(.title3)
+                            .padding(.horizontal)
                             .foregroundColor(.white)
-                            .padding()
-                            .background(Color.orange)
-                            .cornerRadius(10)
-                            .shadow(color: .orange.opacity(0.8), radius: 10, x: 5, y: 5)
-                    })
-                    .padding(.bottom, 20)
+                    }
                     Spacer()
-                    Text(resultText)
-                        .multilineTextAlignment(.center)
-                        .font(.title3)
-                        .padding(.horizontal)
+                    HStack() {
+                        Image(uiImage: compressImage(image: UIImage(imageLiteralResourceName: "SOB")))
+                            .resizable()
+                            .frame(width: 30, height: 30, alignment: .center)
+                            .padding(.bottom, 40)
+                        Text("Summer of Bitcoin")
+                            .font(.footnote)
+                            .foregroundColor(.white)
+                            .padding(.bottom, 40)
+                            .padding(.trailing, 15)
+                        Image(uiImage: compressImage(image: UIImage(imageLiteralResourceName: "BDK")))
+                            .resizable()
+                            .frame(width: 70, height: 20)
+                            .padding(.bottom, 40)
+                    }
                 }
-                Spacer()
-                HStack() {
-                    Image(uiImage: compressImage(image: UIImage(imageLiteralResourceName: "SOB")))
-                        .resizable()
-                        .frame(width: 30, height: 30, alignment: .center)
-                        .padding(.bottom, 40)
-                    Text("Summer of Bitcoin")
-                        .font(.footnote)
+                if coinManager.fetchingData {
+                    Color.black
+                        .opacity(0.7)
+                        .ignoresSafeArea()
+                    
+                    ProgressView()
+                        .progressViewStyle(CircularProgressViewStyle())
+                        .scaleEffect(2)
                         .foregroundColor(.white)
-                        .padding(.bottom, 40)
-                        .padding(.trailing, 15)
-                    Image(uiImage: compressImage(image: UIImage(imageLiteralResourceName: "BDK")))
-                        .resizable()
-                        .frame(width: 70, height: 20)
-                        .padding(.bottom, 40)
+                        .position(x: UIScreen.main.bounds.width / 2, y: UIScreen.main.bounds.height / 2)
                 }
             }
-            if coinManager.fetchingData {
-                Color.black
-                    .opacity(0.7)
-                    .ignoresSafeArea()
-                
-                ProgressView()
-                    .progressViewStyle(CircularProgressViewStyle())
-                    .scaleEffect(2)
-                    .foregroundColor(.white)
-                    .position(x: UIScreen.main.bounds.width / 2, y: UIScreen.main.bounds.height / 2)
-            }
+            .edgesIgnoringSafeArea(.bottom)
         }
-        .edgesIgnoringSafeArea(.bottom)
     }
 }
 
